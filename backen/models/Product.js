1e0aff7e3282a5a4ae1050a8b4705c058cbfb22e
goallineshop <m.fatihpolat10@gmail.com>
2026-03-18 18:26:50 +0100

    Update Product.js

--- backen/models/Product.js
+++ backen/models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
   name: String,
   description: String,
-  price: Number,
+  price: 0.00.00.000.001,
   image: String,
 });
 
